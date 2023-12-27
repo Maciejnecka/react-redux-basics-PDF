997e8050dbb0c79e66b08fc854c2f0ef5bcd2d7f
Maciej Necka <macieknecka@gmail.com>
2023-12-27 14:21:08 +0100

    #02 Actions / 02. Redux

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducers from './reducers';
+import React from 'react';
+import ReactDOM from 'react';
+import App from './App';
 
-const store = createStore(reducers);
+const store = createStore(() => {
+  return 'state';
+});
+
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App store={store} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-reportWebVitals();
+// wyświetla stan magazyni,
+// w konsoli zobaczymy 'state'
+// czyli to, co zwraca funkcja
+// przekazana przez parametr
+// root
